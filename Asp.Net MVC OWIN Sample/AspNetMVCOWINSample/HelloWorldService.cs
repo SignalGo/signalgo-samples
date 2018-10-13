c312aa535ec853aa7afe34cf62417e3f6e53cc85
Ali Yousefi <ali.visual.studio@gmail.com>
2018-10-13 23:55:46 +0330

    add asp.net owin sample

---
+++ Asp.Net MVC OWIN Sample/AspNetMVCOWINSample/HelloWorldService.cs
@@ -0,0 +1,17 @@
+﻿using SignalGo.Shared.DataTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetMVCOWINSample
+{
+    [ServiceContract("HelloWorld", ServiceType.HttpService, InstanceType.SingleInstance)]
+    public class HelloWorldService
+    {
+        public string Hello()
+        {
+            return "Hello SignalGo! " + DateTime.Now;
+        }
+    }
+}
---
+++ Asp.Net MVC OWIN Sample/AspNetMVCOWINSample/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Owin;
+using SignalGo.Server.Owin;
+using SignalGo.Server.ServiceManager;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace AspNetMVCOWINSample
+{
+    public class Startup
+    {
+        private readonly ServerProvider serverProvider = new ServerProvider();
+        public void Configuration(IAppBuilder app)
+        {
+
+            //create your server provider
+            ServerProvider serverProvider = new ServerProvider();
+            //register your server service
+            serverProvider.RegisterServerService<HelloWorldService>();
+            //handle cross origin
+            serverProvider.ProviderSetting.HttpSetting.HandleCrossOriginAccess = true;
+            //add signalgo middlleware
+            app.Use<SignalGo.Server.Owin.SignalGoOwinMiddleware>(serverProvider);
+
+            HttpConfiguration config = new HttpConfiguration();
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+            config.Routes.MapHttpRoute(
+           "DefaultApi", // Route name
+           "",
+          "http://localhost:10012/HellowWorld/Hello"        // URL with parameters
+                                                             //new { controller = "HellowWorld", action = "Hello" }  // Parameter defaults
+       );
+            app.UseWebApi(config);
+        }
+    }
+}
